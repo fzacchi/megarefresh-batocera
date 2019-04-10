e16c3a1332580b54136b917f2b42db0073776991
fzacchi <47987718+fzacchi@users.noreply.github.com>
2019-04-11 01:45:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,10 @@ chmod +x megarefresh-onstart.sh
 chmod +x megarefresh-onend.sh
 
 cp "es_systems.cfg" "/usr/share/batocera/datainit/system/.emulationstation/"
-sed -i 's/videoMode.changeMode(wantedGameMode)/#videoMode.changeMode(wantedGameMode)/g' /usr/lib/python2.7/site-packages/configgen/emulatorlauncher.py
+
+if ! grep "#videoMode.changeMode(wantedGameMode)" /usr/lib/python2.7/site-packages/configgen/emulatorlauncher.py ; then
+	sed -i 's/videoMode.changeMode(wantedGameMode)/#videoMode.changeMode(wantedGameMode)/g' /usr/lib/python2.7/site-packages/configgen/emulatorlauncher.py
+fi
 
 /recalbox/scripts/recalbox-save-overlay.sh
 
